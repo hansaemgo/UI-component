9b69f3ebe4a3880c475f9f367e17df5ce2f1bdb8
hansaemgo <rhgkstoa7@gmail.com>
2020-01-09 19:39:24 +0900

    v-if  / isError 사용으로 유효성 검사

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -11,6 +11,7 @@
       </div>
       <button type="submit">로그인</button>
     </form>
+    <p v-if="isError">올바르지 않은 ID입니다.</p>
   </div>
 </template>
 
@@ -20,13 +21,21 @@ export default {
       return {
         username: '',
         password: '',
+        isError: false,
       };
     },
     methods: {
-      submitForm(event) {
+      submitForm() {
         // event.preventDefault(); form->submit.prevent로 사용 가능
         console.log('로그인');
-        this.initForm();
+        // axios
+        //   .post()
+        //   .then()
+        //   .catch(error => {
+        //     this.isError = true;
+        //   });
+        this.isError = true;
+        //this.initForm();
       },
       initForm() {
         this.username = "";
